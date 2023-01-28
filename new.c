bcfcd8a16c7ad4deb56580ef14dc09bb54401f7a
nagaraj <neelamnagaraj99@gmail.com>
2023-01-28 08:47:06 +0530

    commit on 28012023

--- new.c
+++ new.c
@@ -1,21 +1,47 @@
+//program to check a string is a anagram or not
+//program done by sorting of strings
 #include<stdio.h>
-
+#include<string.h>
 int main() {
-    int i,n,c;
-    char str[100];
-    char key[100];
-    printf("Enter how many strings(names):");
-    scanf("%d",&n);
-    for(i=0;i<n;i++) {
-      scanf("%s",str);
-    }
-    printf("Enter a string to search:");
-    scanf("%s",key);
-    for(i=0;i<n;i++) {
-        if(strcmp(key==str[i])==0) {
-            c=i;
+    char a[100];
+    char b[100];
+    char t;
+    int l1=0,l2=0,flag=0,c=0;
+    printf("Enter string 1:");
+     scanf("%s",a);
+     printf("Enter string 2:");
+     scanf("%s",b);
+     l1=strlen(a);
+     l2=strlen(b);
+     if(l1==l2){
+        //sorting of string1
+     for(int i=0;i<l1;i++) {
+        for(int j=0;i<l1-i;i++) {
+        if(a[i]>a[i+1]){
+            t=a[i];
+            a[i]=a[i+1];
+            a[i+1]=t;
         }
-    }
-    printf("Found at %d",c);
+     }
+     }
+     //sorting of string 2
+     for(int i=0;i<l2;i++) {
+        for(int j=0;i<l2-i;i++) {
+        if(b[i]>b[i+1]){
+            t=b[i];
+            b[i]=b[i+1];
+            b[i+1]=t;
+        }
+     }
+     }
+     //checking both are equal or not
+     if(strcmp(a,b)==0)
+       {
+        printf("Anagram");
+       }
+     }
+     if(l1!=l2) {
+        printf("Not an anagram");
+     }
     return 0;
 }
